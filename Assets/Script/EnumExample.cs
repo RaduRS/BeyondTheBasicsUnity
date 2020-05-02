86ab7dd13cda870499f5eba58f56372d32b78e27
RaduRS <rsrusu90@gmail.com>
2020-05-02 13:07:52 +0100

    adding code

---
+++ Assets/Script/EnumExample.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumExample : MonoBehaviour
+{
+
+    private GameState gameState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = GameState.Running;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.Ended:
+                Debug.Log("Game ended");
+                break;
+            case GameState.Running:
+                Debug.Log("running game");
+                break;
+            case GameState.Pause:
+                Debug.Log("pausing");
+                break;
+        }
+    }
+}
+
+public enum GameState
+{
+    Running, Pause, Ended
+}
---
+++ Assets/Script/SampleScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SampleScript : MonoBehaviour
+{
+    #region DeclaringVariables
+
+    public int numberToDisplay;
+    public int numberToMultiplyBy = 6;
+
+    #endregion DeclaringVariables
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        SayHello();
+        MultiplyNumber(5);
+        Anonim();
+    }
+
+    private void SayHello()
+    {
+        var randomNumber = Random.Range(0, numberToDisplay);
+        var displayText = "Heellloo";
+
+        for(int i = 0; i < randomNumber; i++)
+        {
+            Debug.Log(displayText);
+        }
+    }
+
+    private void MultiplyNumber(int numberToMultiply)
+    {
+        int product = numberToMultiply * numberToMultiplyBy;
+
+        Debug.Log(product);
+    }
+
+    private void Anonim()
+    {
+        var enemy = new[] { new{ name = "Monster", hitPoints = 100 },
+                            new{ name = "Goblin", hitPoints = 150 } };
+
+        Debug.Log(enemy[1].name);
+
+        foreach(var enem in enemy)
+        {
+            Debug.Log(enem.name + enem.hitPoints);
+        }
+    }
+}
